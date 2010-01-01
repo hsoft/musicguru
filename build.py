c9245a31dfd30b50f052107010662c92e6ea112b
Virgil Dupras <hsoft@hardcoded.net>
2010-01-01 20:32:06 +0100

    Fixed build process, which was broken (it was including everything in ".hg" folders in the cocoa plugin's resources). Required quite some mess-around...

--- build.py
+++ build.py
@@ -2,7 +2,7 @@
 # Created By: Virgil Dupras
 # Created On: 2009-12-31
 # $Id$
-# Copyright 2009 Hardcoded Software (http://www.hardcoded.net)
+# Copyright 2010 Hardcoded Software (http://www.hardcoded.net)
 # 
 # This software is licensed under the "HS" License as described in the "LICENSE" file, 
 # which should be included with this package. The terms are also available at 
@@ -11,12 +11,13 @@
 import sys
 import os
 import os.path as op
+import shutil
 
+from setuptools import setup
 import yaml
 
 from hsdocgen import generate_help, filters
-from hsutil.build import (move_testdata_out, put_testdata_back, add_to_pythonpath,
-    print_and_do, build_all_qt_ui)
+from hsutil.build import add_to_pythonpath, print_and_do, build_all_qt_ui, copy_packages
 
 def main():
     conf = yaml.load(open('conf.yaml'))
@@ -34,22 +35,39 @@ def main():
     destpath = op.abspath(op.join('help', 'musicguru_help'))
     generate_help.main(basepath, destpath, force_render=not dev, tix=tix, windows=windows)
     if ui == 'cocoa':
-        move_log = move_testdata_out()
-        try:
-            os.chdir('cocoa')
-            if dev:
-                os.system('python gen.py --dev')
-            else:
-                os.system('python gen.py')
-            os.chdir('..')
-        finally:
-            put_testdata_back(move_log)
+        if not dev:
+            print "Building help index"
+            help_path = op.abspath('help/musicguru_help')
+            os.system('open /Developer/Applications/Utilities/Help\\ Indexer.app --args {0}'.format(help_path))
+        
+        print "Building mg_cocoa.plugin"
+        if op.exists('build'):
+            shutil.rmtree('build')
+        os.mkdir('build')
+        if not dev:
+            copy_packages(['core', 'hsutil', 'hsmedia', 'hsfs'], 'build')
+        shutil.copy('cocoa/mg_cocoa.py', 'build')
+        os.chdir('build')
+        script_args = ['py2app', '-A'] if dev else ['py2app']
+        setup(
+            script_args = script_args,
+            plugin = ['mg_cocoa.py'],
+            setup_requires = ['py2app'],
+        )
+        os.chdir('..')
+        if op.exists('cocoa/mg_cocoa.plugin'):
+            shutil.rmtree('cocoa/mg_cocoa.plugin')
+        shutil.move('build/dist/mg_cocoa.plugin', 'cocoa/mg_cocoa.plugin')
         if dev:
             # In alias mode, the tweakings we do to the pythonpath aren't counted in. We have to
             # manually put a .pth in the plugin
-            pluginpath = 'cocoa/build/Release/musicGuru.app/Contents/Resources/mg_cocoa.plugin'
+            pluginpath = 'cocoa/mg_cocoa.plugin'
             pthpath = op.join(pluginpath, 'Contents/Resources/dev.pth')
             open(pthpath, 'w').write(op.abspath('.'))
+        os.chdir('cocoa')
+        print "Building the XCode project"
+        os.system('xcodebuild')
+        os.chdir('..')
     elif ui == 'qt':
         build_all_qt_ui(op.join('qtlib', 'ui'))
         build_all_qt_ui(op.join('qt', 'ui'))
