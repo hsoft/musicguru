f1337bebbbdf408ffbe4e52f00d6c653d91b9e6d
Virgil Dupras <hsoft@hardcoded.net>
2010-04-09 09:09:02 +0200

    Removed old cocoalib and added it as a subrepo.

--- cocoalib/brsinglelineformatter/NSCharacterSet_Extensions.h
+++
@@ -1,6 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-// Only needed for Tiger. Remove when targeting the 10.5 SDK
-@interface NSCharacterSet (NewLineCharacterSet)
-+ (NSCharacterSet *)newlineCharacterSet;
-@end
